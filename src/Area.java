f2dc3746d31c72e86a02a05c933e467be047c771
Casper Henningsson <cabbe.h01@gmail.com>
2021-05-02 18:24:05 +0200

    Area draw panel

--- src/Area.java
+++ src/Area.java
@@ -1,3 +1,77 @@
-public class Area {
-    //Tjena
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Area extends JPanel {
+    //Creating random object
+    Random r = new Random();
+
+    //Position for the "new" ball object
+    int x,y;
+
+    //Variable to see if the game is paused
+    public boolean paused = false;
+
+
+    //This method is the heart of the program.
+    //It loops through each frame to update simulation and calls the redraw function
+    public void gameloop(){
+        //Constants
+        final int TICKSPERSECONDS = 64;
+        final int SKIPTICK = 1000 / TICKSPERSECONDS;
+        final int MAXFRAMESKIP = 10;
+
+        //Getting the current time in milliseconds
+        double ngt = System.currentTimeMillis();
+
+        //Amount of iterations
+        int iterations;
+
+        //Infinity loop
+        while (true) {
+            iterations = 0;
+            //Checks so the time is running forward and checks so the iteration is lower than the frameskip
+            while (System.currentTimeMillis() > ngt && iterations < MAXFRAMESKIP) {
+                //Skips to redraw if the simulation is paused
+                if(paused){
+                    continue;
+                }
+                else{
+                    //Checking collision
+
+                    //Repaints
+                    this.repaint();
+
+                }
+                //Adding the SkipTicks
+                ngt += SKIPTICK;
+
+                //Count +1 on iteration
+                iterations++;
+            }
+        }
+    }
+
+
+    //The paint component to draw the panel and calling the paint function on the balls
+    @Override
+    public void paint(Graphics g) {
+
+        //Getting the properties from the original paint class so
+        //so we just adding our code to it and don't re writes it
+        super.paint(g);
+
+        Graphics2D g2d = (Graphics2D)g;
+
+        this.setBackground(Color.WHITE);
+
+        //Enabling antialias to get a smoother experience
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        g2d.setColor(Color.RED);
+
+        //Drawing the contour
+        g2d.fillOval(998,950,1,1);
+    }
 }
--- src/MapCreator.java
+++ src/MapCreator.java
@@ -1,18 +1,30 @@
-import javax.sound.midi.Soundbank;
 import javax.swing.*;
 import java.awt.*;
 import java.io.FileNotFoundException;
 import java.sql.SQLOutput;
+import java.io.File;
 
 public class MapCreator {
+
+
     public static void main(String[] args) {
         JFrame map = new JFrame("Map");
         map.setPreferredSize(new Dimension(1000,1000));
 
-        map.add(new Map());
-        map.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
         map.pack();
+        map.setVisible(true);
+        //System.out.println(map.getHeight());
+
+        JPanel area = new Area();
+
+        area.setPreferredSize(new Dimension(map.getWidth(),map.getHeight()));
+        map.add(area);
+        System.out.println();
+
+
+        map.setTitle("Shortest Path");
+        map.setResizable(false);
+        map.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         map.setVisible(true);
         //System.out.println(map);
 
@@ -21,13 +33,8 @@ public class MapCreator {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+        //Icon to the program
+        //map.setIconImage(ImageIO.read(new File("res/icon.png")));
     }
 
-}
-class Map extends JPanel {
-    public Map() {
-        this.setBackground(Color.white);
-
-
-    }
 }
