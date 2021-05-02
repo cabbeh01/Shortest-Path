8e8c854ffecac36e096282ad2f97a0a48a4993ff
DrCraz <leobabiclego@gmail.com>
2021-05-02 18:35:59 +0200

    Skapade polygon

--- src/Polygon.java
+++ src/Polygon.java
@@ -1,2 +1,21 @@
 public class Polygon {
+    int[] xarray;
+    int[] yarray;
+    int npoint;
+
+    public Polygon(int[] xarray, int[] yarray, int npoint){
+        this.xarray = xarray;
+        this.yarray = yarray;
+        this.npoint = npoint;
+    }
+
+    public int[] getX() {
+        return xarray;
+    }
+
+    public int[] getY() {
+        return yarray;
+    }
+
+    public int getNpoint() {return npoint; }
 }
