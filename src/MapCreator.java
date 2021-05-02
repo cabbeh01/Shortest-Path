73772beee9077818f63b6cf3f5a7ce5683d352fe
DrCraz <leobabiclego@gmail.com>
2021-05-02 17:55:09 +0200

    Skapade Map classen med JFrame

--- src/MapCreator.java
+++ src/MapCreator.java
@@ -6,9 +6,16 @@ public class MapCreator {
         JFrame map = new JFrame("Map");
         map.setPreferredSize(new Dimension(1000,1000));
 
+        map.add(new Map());
         map.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         map.pack();
         map.setVisible(true);
+    }
+
+}
+class Map extends JPanel {
+    public Map() {
+        this.setBackground(Color.white);
     }
 }
